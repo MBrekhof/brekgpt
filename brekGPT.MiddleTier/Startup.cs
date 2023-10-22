fb9a6dc5a5279aaf3a7bf679108346d664fe5da8
Martin Brekhof <martin@brekhof.nl>
2023-10-22 14:07:03 +0200

    start

---
+++ brekGPT.MiddleTier/Startup.cs
@@ -0,0 +1,176 @@
+﻿using DevExpress.ExpressApp.Security;
+using DevExpress.Persistent.Base;
+using Microsoft.EntityFrameworkCore;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.EntityFrameworkCore.Security.MiddleTier.ClientServer;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using brekGPT.WebApi.JWT;
+using DevExpress.ExpressApp.Security.Authentication.ClientServer;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+
+namespace brekGPT.WebApi;
+
+public class Startup {
+    public Startup(IConfiguration configuration) {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddScoped<IAuthenticationTokenProvider, JwtTokenProviderService>();
+
+        services.AddXafMiddleTier(Configuration, builder => {
+
+            builder.Modules
+                .AddReports(options => {
+                    options.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.EF.ReportDataV2);
+                })
+                .Add<brekGPT.Module.brekGPTModule>();
+
+
+            builder.ObjectSpaceProviders
+                .AddSecuredEFCore()
+                    .WithDbContext<brekGPT.Module.BusinessObjects.brekGPTEFCoreDbContext>((serviceProvider, options) => {
+                        // Uncomment this code to use an in-memory database. This database is recreated each time the server starts. With the in-memory database, you don't need to make a migration when the data model is changed.
+                        // Do not use this code in production environment to avoid data loss.
+                        // We recommend that you refer to the following help topic before you use an in-memory database: https://docs.microsoft.com/en-us/ef/core/testing/in-memory
+                        //options.UseInMemoryDatabase("InMemory");
+                        string connectionString = null;
+                        if(Configuration.GetConnectionString("ConnectionString") != null) {
+                            connectionString = Configuration.GetConnectionString("ConnectionString");
+                        }
+#if EASYTEST
+                        if(Configuration.GetConnectionString("EasyTestConnectionString") != null) {
+                            connectionString = Configuration.GetConnectionString("EasyTestConnectionString");
+                        }
+#endif
+                        ArgumentNullException.ThrowIfNull(connectionString);
+                        options.UseSqlServer(connectionString);
+                        options.UseChangeTrackingProxies();
+                        options.UseObjectSpaceLinkProxies();
+                        options.UseLazyLoadingProxies();
+                    })
+                .AddNonPersistent();
+
+            builder.Security
+                .UseIntegratedMode(options => {
+                    options.RoleType = typeof(PermissionPolicyRole);
+                    // ApplicationUser descends from PermissionPolicyUser and supports the OAuth authentication. For more information, refer to the following topic: https://docs.devexpress.com/eXpressAppFramework/402197
+                    // If your application uses PermissionPolicyUser or a custom user type, set the UserType property as follows:
+                    options.UserType = typeof(brekGPT.Module.BusinessObjects.ApplicationUser);
+                    // ApplicationUserLoginInfo is only necessary for applications that use the ApplicationUser user type.
+                    // If you use PermissionPolicyUser or a custom user type, comment out the following line:
+                    options.UserLoginInfoType = typeof(brekGPT.Module.BusinessObjects.ApplicationUserLoginInfo);
+                    options.Events.OnSecurityStrategyCreated += securityStrategy => {
+                        //((SecurityStrategy)securityStrategy).AnonymousAllowedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.EF.ModelDifference));
+                        //((SecurityStrategy)securityStrategy).AnonymousAllowedTypes.Add(typeof(DevExpress.Persistent.BaseImpl.EF.ModelDifferenceAspect));
+                        ((SecurityStrategy)securityStrategy).PermissionsReloadMode = PermissionsReloadMode.CacheOnFirstAccess;
+                    };
+                })
+                .AddPasswordAuthentication(options => {
+                    options.IsSupportChangePassword = true;
+                });
+
+            builder.AddBuildStep(application => {
+                application.ApplicationName = "SetupApplication.brekGPT";
+                application.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+                //application.CreateCustomModelDifferenceStore += += (s, e) => {
+                    //    e.Store = new ModelDifferenceDbStore((XafApplication)sender!, typeof(ModelDifference), true, "Win");
+                    //    e.Handled = true;
+                //};
+#if DEBUG
+                if(System.Diagnostics.Debugger.IsAttached && application.CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+                    application.DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+                    application.DatabaseVersionMismatch += (s, e) => {
+                        e.Updater.Update();
+                        e.Handled = true;
+                    };
+                }
+#endif
+            });
+        });
+
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters() {
+                    ValidateIssuerSigningKey = true,
+                    //ValidIssuer = Configuration["Authentication:Jwt:Issuer"],
+                    //ValidAudience = Configuration["Authentication:Jwt:Audience"],
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Authentication:Jwt:IssuerSigningKey"]))
+                };
+            });
+
+        services.AddAuthorization(options => {
+            options.DefaultPolicy = new AuthorizationPolicyBuilder(
+                JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .RequireXafAuthentication()
+                    .Build();
+        });
+
+        services.AddSwaggerGen(c => {
+            c.EnableAnnotations();
+            c.SwaggerDoc("v1", new OpenApiInfo {
+                Title = "brekGPT API",
+                Version = "v1",
+                Description = @"MiddleTier"
+            });
+            c.AddSecurityDefinition("JWT", new OpenApiSecurityScheme() {
+                Type = SecuritySchemeType.Http,
+                Name = "Bearer",
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme() {
+                            Reference = new OpenApiReference() {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "JWT"
+                            }
+                        },
+                        new string[0]
+                    },
+            });
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        if(env.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "brekGPT Api v1");
+            });
+        }
+        else {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. To change this for production scenarios, see: https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseRequestLocalization();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseXafMiddleTier();
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllers();
+        });
+    }
+}
